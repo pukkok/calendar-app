46bbb7e879871b6adc836c8d89185680656927a6
pukkok <krystalgggg@gmail.com>
2024-11-29 11:52:26 +0900

    닫히면 종료하는 기능 추가

--- main.js
+++ main.js
@@ -2,15 +2,20 @@ import { app, BrowserWindow } from 'electron'
 
 // 재사용 가능한 기능을 작성하여 창을 인스턴스화하기
 const createWindow = () => {
-    const win = new BrowserWindow({
-        width: 800,
-        height: 600
-    })
+	const win = new BrowserWindow({
+		width: 800,
+		height: 600
+	})
 
-    win.loadFile('index.html')
+	win.loadFile('index.html')
 }
 
 // 앱이 준비되면 기능 호출
 app.whenReady().then(() => {
-    createWindow()
+	createWindow()
+})
+
+// 모든 창이 닫히면 앱 종료(Windows 및 Linux)
+app.on('window-all-closed', () => {
+	if (process.platform !== 'darwin') app.quit()
 })
