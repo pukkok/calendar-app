f3c492b7c8a614c63f8ac021b9fefd536f8a0375
pukkok <krystalgggg@gmail.com>
2024-11-29 11:49:59 +0900

    index.html electron으로 실행 시키기

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-console.log('hello electron')
+import { app, BrowserWindow } from 'electron'
+
+// 재사용 가능한 기능을 작성하여 창을 인스턴스화하기
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600
+    })
+
+    win.loadFile('index.html')
+}
+
+// 앱이 준비되면 기능 호출
+app.whenReady().then(() => {
+    createWindow()
+})
