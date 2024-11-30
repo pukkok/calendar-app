c3868f3be69da988129c0a6fb3b331bff1ac35af
Pukkok <krystalgggg@gmail.com>
2024-12-01 02:26:53 +0900

    startWeekIndex, totalMonthInDays 테스트 완료

--- src/calendar/Calendar.tsx
+++ src/calendar/Calendar.tsx
@@ -1,10 +1,17 @@
+import { useState } from "react"
 import FlexiDate from "./lib/FlexiDate"
 
 const Calendar = () => {
 
-  const today = new FlexiDate(2024, 12, 2)
-  console.log(today.format('YYYY-MM-DD hh:mm'))
-  console.log(today.weekDay())
+  const today = new FlexiDate(2024, 11, 1)
+
+  const [currentDate, setCurrentDate] = useState(today)
+  const startWeekIndex = currentDate.startWeekIndex
+  const totalDays = currentDate.totalMonthInDays
+  
+  console.log(totalDays)
+  console.log(startWeekIndex)
+  
 
   return (
     <>달력 만들기</>
