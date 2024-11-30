5b12f969286eb63894e563aa28232eb01a96939a
Pukkok <krystalgggg@gmail.com>
2024-12-01 01:15:50 +0900

    week 객체로 변경

--- src/calendar/constants/week.ts
+++ src/calendar/constants/week.ts
@@ -1,2 +1,6 @@
-export const koWeek = ["일", "월", "화", "수", "목", "금", "토"]
-export const enWeek = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"]
+const week = {
+  "ko" : ["일", "월", "화", "수", "목", "금", "토"],
+  "en" : ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"]
+}
+
+export default week
