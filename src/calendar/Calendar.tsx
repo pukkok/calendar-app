5ed6c7de9133683c149962461c201ff169935a2c
Pukkok <krystalgggg@gmail.com>
2024-12-01 00:57:46 +0900

    EnhancedDate 테스트 완료

--- src/calendar/Calendar.tsx
+++ src/calendar/Calendar.tsx
@@ -1,6 +1,10 @@
-
+import EnhancedDate from "./lib/EnhancedDate"
 
 const Calendar = () => {
+
+  const today = new EnhancedDate()
+  console.log(today.date)
+  const day = new Date(24, 11, 1)
 
   return (
     <>달력 만들기</>
