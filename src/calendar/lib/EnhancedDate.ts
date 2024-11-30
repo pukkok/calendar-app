5ad08853cfc5509b5beeca63c98593d18389298f
Pukkok <krystalgggg@gmail.com>
2024-12-01 00:57:18 +0900

    필요한 Date 객체 커스텀

---
+++ src/calendar/lib/EnhancedDate.ts
@@ -0,0 +1,45 @@
+class EnhancedDate {
+  private _date: Date // 월은 0부터 시작
+
+  constructor(
+    year?: number, 
+    month?: number, 
+    day?: number,
+    hours? : number,
+    minutes? : number
+  ) {
+    if(!year) this._date = new Date()
+    else if(!month) this._date = new Date(year)
+    else if(!day) this._date = new Date(year, month-1) 
+    else if(!hours) this._date = new Date(year, month-1, day)
+    else if(!minutes) this._date = new Date(year, month-1, day, hours)
+    else this._date = new Date(year, month - 1, day, hours, minutes) 
+  }
+
+  get all () : object {
+    return this._date
+  }
+
+  get date () : number {
+    return this._date.getDate()
+  }
+
+  get minutes () : number {
+    return this._date.getMinutes()
+  }
+  get hours () : number {
+    return this._date.getHours()
+  }
+  
+  get day () : number {
+    return this._date.getDay()
+  }
+  get month () : number {
+    return this._date.getMonth()
+  }
+  get year () : number {
+    return this._date.getFullYear()
+  }
+}
+
+export default EnhancedDate
