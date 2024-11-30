91f50da6c19de48344621a1148539cdb42ed4f4e
Pukkok <krystalgggg@gmail.com>
2024-12-01 01:05:41 +0900

    ko, en 버전 일주일 표기 배열 생성

--- src/calendar/constants/week.ts
+++ src/calendar/constants/week.ts
@@ -1,1 +1,2 @@
-export const week = ["일", "월", "화", "수", "목", "금", "토"]
+export const koWeek = ["일", "월", "화", "수", "목", "금", "토"]
+export const enWeek = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"]
