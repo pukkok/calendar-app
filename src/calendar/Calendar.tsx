8aeccca094027af62df63c793abe20e00221861c
Pukkok <krystalgggg@gmail.com>
2024-12-01 01:19:19 +0900

    weekDay 메서드 테스트 완료

--- src/calendar/Calendar.tsx
+++ src/calendar/Calendar.tsx
@@ -2,8 +2,9 @@ import FlexiDate from "./lib/FlexiDate"
 
 const Calendar = () => {
 
-  const today = new FlexiDate()
+  const today = new FlexiDate(2024, 12, 2)
   console.log(today.format('YYYY-MM-DD hh:mm'))
+  console.log(today.weekDay())
 
   return (
     <>달력 만들기</>
