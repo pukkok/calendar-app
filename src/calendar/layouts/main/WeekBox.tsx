3eb29faaae45b20900a59ee538ccf564efc28a6d
Pukkok <krystalgggg@gmail.com>
2024-12-01 18:59:57 +0900

    calendar 기본 폼 설정

--- src/calendar/layouts/main/WeekBox.tsx
+++ src/calendar/layouts/main/WeekBox.tsx
@@ -1,13 +1,29 @@
-import DayBox from "./DayBox"
+import DataBox from "./DataBox"
 import WeekTop from "./WeekTop"
 
-const WeekBox = () => {
+interface mainProps {
+  monthArray: number[][]
+}
+
+const WeekBox = ({ monthArray } : mainProps) => {
 
   return (
-  <div>
-    <WeekTop></WeekTop>
-    <DayBox></DayBox>
-  </div>
+  <>
+    {monthArray.map((weekArray, weekIdx) => {
+      return <div className="week-box" key={weekIdx}>
+        {weekArray.map((date, dateIdx) => {
+          return (
+            <div key={dateIdx}>
+              <WeekTop>{date}</WeekTop>
+              <DataBox>
+                <p>데이터 {dateIdx}</p>
+              </DataBox>
+            </div>
+          )
+        })}
+      </div>
+    })}
+  </>
   )
 }
 
