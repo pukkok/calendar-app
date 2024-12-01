226d9809cfb920869fa5a109427d540686c4f4f9
Pukkok <krystalgggg@gmail.com>
2024-12-01 20:39:13 +0900

    headline, navigateButton 연결 테스트 완료

--- src/calendar/layouts/Header.tsx
+++ src/calendar/layouts/Header.tsx
@@ -1,6 +1,13 @@
+import HeadLine from "./header/HeadLine"
+import NavigateButton from "./header/NavigateButton"
+
+
 const Header = () => {
 
-  return <header>달력 헤더 부분</header>
+  return <header>
+    <HeadLine/>
+    <NavigateButton></NavigateButton>
+  </header>
 }
 
 export default Header
